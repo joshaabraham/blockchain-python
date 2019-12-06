a30cdcbe54ebf25250f8b0793a8164682973dda6
Remi Duplan <duplan.remi@gmail.com>
2019-12-06 13:39:59 -0500

    ajout d'une structure

---
+++ models/article.py
@@ -0,0 +1,9 @@
+class  Article:
+    
+    def __init__(self, titre, contenu, auteur, date):
+        self.titre = titre
+        self.contenu = contenu
+        self.auteur = auteur
+        self.date = date
+
+    
